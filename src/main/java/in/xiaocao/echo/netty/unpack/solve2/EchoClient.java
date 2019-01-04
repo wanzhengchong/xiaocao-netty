63a6ebcc11191c542b91ee014e212eeab77cc830
WanZhengchong <wanzhengchong@163.com>
2019-01-04 20:17:02 +0800

    使用固定分隔符来解决拆包粘包问题

---
+++ src/main/java/in/xiaocao/echo/netty/unpack/solve2/EchoClient.java
@@ -0,0 +1,33 @@
+package in.xiaocao.echo.netty.unpack.solve2;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class EchoClient {
+    public void run() throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup(); // 创建一个线程池
+        try {
+            Bootstrap client = new Bootstrap(); // 创建客户端处理程序
+            client.group(group).channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new EchoClientHandler()); // 追加了处理器
+                        }
+                    });
+            ChannelFuture channelFuture = client.connect("localhost", 9999).sync();
+            channelFuture.channel().closeFuture().sync(); // 关闭连接
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+		new EchoClient().run();
+	}
+}
---
+++ src/main/java/in/xiaocao/echo/netty/unpack/solve2/EchoServerHandler.java
@@ -0,0 +1,24 @@
+package in.xiaocao.echo.netty.unpack.solve2;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+import io.netty.util.ReferenceCountUtil;
+
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+	
+	private int index = 0;
+	
+	@Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        try {
+        	ByteBuf buf = (ByteBuf) msg;
+        	String inputData = buf.toString(CharsetUtil.UTF_8);
+        	System.out.println((index ++) + inputData);
+        } finally {
+            ReferenceCountUtil.release(msg) ; // 释放缓存
+        }
+    }
+
+}
