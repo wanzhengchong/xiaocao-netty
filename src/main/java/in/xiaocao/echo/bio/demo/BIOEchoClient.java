ba5a13a8cf2e3465d0fd99214345b2f2061b0cef
WanZhengchong <wanzhengchong@163.com>
2019-01-04 19:12:34 +0800

    同步阻塞式io实现

--- src/main/java/in/xiaocao/echo/bio/demo/BIOEchoClient.java
+++ src/main/java/in/xiaocao/echo/bio/demo/BIOEchoClient.java
@@ -6,25 +6,33 @@ import java.util.Scanner;
 
 import in.xiaocao.echo.util.InputUtil;
 
+/**
+ * 
+ * @ClassName: BIOEchoClient
+ * @Description: 阻塞式io的客户端
+ * @author zhengchong.wan
+ * @date 2019年1月4日 下午7:10:37
+ *
+ */
 public class BIOEchoClient {
-    public static void main(String[] args) throws Exception {
-        Socket client = new Socket("localhost", 9999) ;  // 定义连接的主机信息
-        Scanner scan = new Scanner(client.getInputStream()) ;   // 获取服务器端的响应数据
-        scan.useDelimiter("\n") ;
-        PrintStream out = new PrintStream(client.getOutputStream()) ; // 向服务器端发送信息内容
-        boolean flag = true ; // 交互的标记
-        while(flag) {
-            String inputData = InputUtil.getString("请输入要发送的内容：").trim() ;
-            out.println(inputData); // 把数据发送到服务器端上
-            if(scan.hasNext()) {
-                String str = scan.next().trim() ;
-                System.out.println(str);
-            }
-            if ("byebye".equalsIgnoreCase(inputData)) {
-                flag = false ;
-            }
-        }
-        client.close();
-        scan.close();
-    }
+	public static void main(String[] args) throws Exception {
+		Socket client = new Socket("localhost", 9999); // 定义连接的主机信息
+		Scanner scan = new Scanner(client.getInputStream()); // 获取服务器端的响应数据
+		scan.useDelimiter("\n");
+		PrintStream out = new PrintStream(client.getOutputStream()); // 向服务器端发送信息内容
+		boolean flag = true; // 交互的标记
+		while (flag) {
+			String inputData = InputUtil.getString("请输入要发送的内容：").trim();
+			out.println(inputData); // 把数据发送到服务器端上
+			if (scan.hasNext()) {
+				String str = scan.next().trim();
+				System.out.println(str);
+			}
+			if ("byebye".equalsIgnoreCase(inputData)) {
+				flag = false;
+			}
+		}
+		client.close();
+		scan.close();
+	}
 }
